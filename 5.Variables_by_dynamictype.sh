975798a218378f1f7b8e7baaf4f830de64c9c627
Sai Swetha Paruchuri <psswetha2001@gmail.com>
2026-02-03 11:44:26 +0530

    Passing variables dynamically

---
+++ 5.Variables_by_dynamictype.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Now we are writing a shell scripting to pass the variables in a dynamic way, which means we are taking input from user from teriminal and save it in read variable and then we can reuse it in any where
+
+#see below script
+echo " Hi user , Can you enter your name : "
+read User_Name
+echo " The name of the user is $User_Name "
+
