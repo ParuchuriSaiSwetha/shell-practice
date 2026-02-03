dc5cd2f3fa91aba7cf94d13ddc12cd6e382b6e56
Sai Swetha Paruchuri <psswetha2001@gmail.com>
2026-02-03 11:36:30 +0530

    Passing variables with arguments while running the Script

---
+++ 4.Arugument_Variables.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+Person1=$1
+Person2=$2
+echo "$Person1: Hi $Person2, How are you"
+echo "$Person2 : Hello Dude, I am doing good how about you $Person1"
+echo "$Person1 : Good, Will call in sometime $Person2"
