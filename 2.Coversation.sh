73a593d49281312fef5bea2398d289ee671ad0f1
Sai Swetha Paruchuri <psswetha2001@gmail.com>
2026-02-03 11:29:10 +0530

    Variables

--- 2.Coversation.sh
+++
@@ -1,4 +0,0 @@
-#!bin/bash
-echo "Sweth: Hi Swe, How are you"
-echo "Swe : Hello Dude, I am doing good how about you"
-echo "Sweth : Good"
---
+++ 3.Variables.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+Person1=Swetha
+Person2=Swe
+echo "$Person1: Hi $Person2, How are you"
+echo "$Person2 : Hello Dude, I am doing good how about you $Person1"
+echo "$Person1 : Good, Will call in sometime $Person2"
